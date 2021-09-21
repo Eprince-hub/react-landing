f2e4301e55acfb20cd0d42790575ca44d28b5c0f
VICTOR EJIKE NWOSU <74430629+Eprince-hub@users.noreply.github.com>
2021-09-21 14:28:33 +0200

    create about page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import AboutPage from './component/AboutPage';
 import HeroPage from './component/HeroPage';
 import Navigation from './component/Navigation';
 import TestingPage from './component/TestingPage';
@@ -8,16 +9,17 @@ import TestingPage from './component/TestingPage';
 // import logo from './logo.svg';
 
 const appStyle = css`
-min-height: 100vh;
-background-color: #0C022F;
-color: white;
-`
+  min-height: 100vh;
+  background-color: #0c022f;
+  color: white;
+`;
 
 function App() {
   return (
     <div css={appStyle}>
       <Navigation />
       <HeroPage />
+      <AboutPage />
       <TestingPage />
     </div>
   );
---
+++ src/component/AboutPage.js
@@ -0,0 +1,22 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+// import cables from './images/cables.svg';
+// import computers from './images/computer.svg';
+// import handLeftRepeat from './images/hand-left-repeat.svg';
+// import handLeft from './images/hand-left.svg';
+// import handRightRepeat from './images/hand-right-repeat.svg';
+// import handRight from './images/hand-right.svg';
+// import heroPageImage from './images/hero-background-svg.svg';
+
+// Regular Css for the Hero page!
+
+const aboutPageStyle = css``;
+
+export default function AboutPage() {
+  return (
+    <section css={aboutPageStyle}>
+      <h1>This is a message from the about page!</h1>
+    </section>
+  );
+}
